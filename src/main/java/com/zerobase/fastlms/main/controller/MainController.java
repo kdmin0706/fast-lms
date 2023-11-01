bfec0797e679cee3620718109d8746cdcf62c90d
kdmin0706 <gxxk76@gmail.com>
2023-11-01 16:24:16 +0900

    메인 화면 배너 추가

--- src/main/java/com/zerobase/fastlms/main/controller/MainController.java
+++ src/main/java/com/zerobase/fastlms/main/controller/MainController.java
@@ -1,42 +1,37 @@
 package com.zerobase.fastlms.main.controller;
 
 
-import com.zerobase.fastlms.components.MailComponents;
-import com.zerobase.fastlms.util.RequestUtils;
+import com.zerobase.fastlms.admin.dto.BannerDto;
+import com.zerobase.fastlms.admin.model.BannerParam;
+import com.zerobase.fastlms.admin.service.BannerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class MainController {
 
-    private final MailComponents mailComponents;
-    
+    private final BannerService bannerService;
+
     @RequestMapping("/")
-    public String index(HttpServletRequest request) {
+    public String index(Model model, BannerParam parameter) {
 
-        String userAgent = RequestUtils.getUserAgent(request);
-        String clientIp = RequestUtils.getClientIP(request);
-
-        log.info(userAgent);
-        log.info(clientIp);
+        List<BannerDto> bannerDtoList = bannerService.showList(parameter);
+        model.addAttribute("list", bannerDtoList);
 
         return "index";
     }
-    
-    
-    
+
     @RequestMapping("/error/denied")
     public String errorDenied() {
         
         return "error/denied";
     }
-    
-    
-    
+
 }
