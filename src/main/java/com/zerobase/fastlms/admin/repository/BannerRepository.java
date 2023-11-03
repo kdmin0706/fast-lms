239026f3cce8406d59b5100763e5f76c22a0ac10
kdmin0706 <gxxk76@gmail.com>
2023-11-03 11:15:17 +0900

    commit

--- src/main/java/com/zerobase/fastlms/admin/repository/BannerRepository.java
+++ src/main/java/com/zerobase/fastlms/admin/repository/BannerRepository.java
@@ -4,9 +4,6 @@ import com.zerobase.fastlms.admin.entity.Banner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface BannerRepository extends JpaRepository<Banner, Long> {
 }
